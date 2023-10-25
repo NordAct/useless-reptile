b0fa04b97c59cbfc609a99784f9564afdf347300
NordAct <73118477+NordAct@users.noreply.github.com>
2023-10-25 17:59:19 +0300

    Multipart crash fix

--- src/main/java/nordmods/uselessreptile/common/mixin/common/ServerWorldMixin.java
+++ src/main/java/nordmods/uselessreptile/common/mixin/common/ServerWorldMixin.java
@@ -2,17 +2,17 @@ package nordmods.uselessreptile.common.mixin.common;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
-import nordmods.uselessreptile.common.entity.multipart.DragonPartsHolder;
+import nordmods.uselessreptile.common.entity.multipart.WorldMultipartHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerWorld.class)
-public abstract class ServerWorldMixin {
+public abstract class ServerWorldMixin implements WorldMultipartHelper {
     @Inject(method = "getDragonPart(I)Lnet/minecraft/entity/Entity;", at = @At("RETURN"), cancellable = true)
     public void getURDragonParts(int id, CallbackInfoReturnable<Entity> cir) {
         Entity entity = cir.getReturnValue();
-        if (entity == null) cir.setReturnValue(DragonPartsHolder.URDragonParts.get(id));
+        if (entity == null) cir.setReturnValue(getPartMap().get(id));
     }
 }
