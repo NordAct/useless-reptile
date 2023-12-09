aade3fe8893644d8e6a4b5b6caf010a0c0f07324
NordAct <73118477+NordAct@users.noreply.github.com>
2023-12-09 21:12:11 +0300

    Bump MC version to 1.20.4

--- src/main/java/nordmods/uselessreptile/common/entity/special/AcidBlastEntity.java
+++ src/main/java/nordmods/uselessreptile/common/entity/special/AcidBlastEntity.java
@@ -1,23 +1,19 @@
 package nordmods.uselessreptile.common.entity.special;
 
-
 import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 import nordmods.primitive_multipart_entities.common.entity.EntityPart;
-import nordmods.uselessreptile.common.init.URConfig;
-import nordmods.uselessreptile.common.init.UREntities;
-import nordmods.uselessreptile.common.init.URSounds;
-import nordmods.uselessreptile.common.init.URStatusEffects;
+import nordmods.uselessreptile.common.init.*;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -26,38 +22,25 @@ import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-
-public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEntity {
+public class AcidBlastEntity extends ProjectileEntity implements GeoEntity {
 
     private int life;
     private final int color = 10085398;
 
     public AcidBlastEntity(EntityType<? extends AcidBlastEntity> entityType, World world) {
         super(entityType, world);
-        pickupType = PersistentProjectileEntity.PickupPermission.DISALLOWED;
     }
 
     public AcidBlastEntity(World world, LivingEntity owner) {
-        super(UREntities.ACID_BLAST_ENTITY, owner, world);
+        super(UREntities.ACID_BLAST_ENTITY, world);
         setOwner(owner);
     }
 
-    @Override
-    protected ItemStack asItemStack() {
-        return null;
-    }
-
-    @Override
     protected void age() {
         ++life;
         if (life >= 100) {
             discard();
         }
-    }
-
-    @Override
-    protected SoundEvent getHitSound() {
-        return URSounds.ACID_SPLASH;
     }
 
     @Override
@@ -74,11 +57,19 @@ public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEn
     }
 
     @Override
+    public void playSound(SoundEvent sound, float volume, float pitch) {
+        if (!isSilent()) getWorld().playSound(getX(), getY(),getZ(), sound, SoundCategory.NEUTRAL, volume, pitch,true);
+    }
+
+    @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
-        setDamage(2 * URConfig.getDamageMultiplier());
-        if (!getWorld().isClient()) spawnEffectCloud();
+        if (getWorld().isClient()) return;
+        Entity target = entityHitResult.getEntity();
+        target.damage(target.getDamageSources().create(URDamageTypes.ACID, getOwner()),2 * URConfig.getDamageMultiplier());
+        spawnEffectCloud();
+        playSound(URSounds.ACID_SPLASH, 1, 1);
         super.onEntityHit(entityHitResult);
-        if (entityHitResult.getEntity() instanceof LivingEntity entity) entity.addStatusEffect(new StatusEffectInstance(URStatusEffects.ACID, 60, 1));
+        if (target instanceof LivingEntity entity) entity.addStatusEffect(new StatusEffectInstance(URStatusEffects.ACID, 60, 1));
         discard();
 
     }
@@ -93,7 +84,7 @@ public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEn
         areaEffectCloudEntity.setRadius(1.0f);
         areaEffectCloudEntity.setDuration(20);
         areaEffectCloudEntity.setRadiusGrowth(0.1f);
-        areaEffectCloudEntity.addEffect(new StatusEffectInstance(URStatusEffects.ACID, 30, 1));
+        areaEffectCloudEntity.addEffect(new StatusEffectInstance(URStatusEffects.ACID, 10, 1));
         areaEffectCloudEntity.setSilent(true);
         getWorld().spawnEntity(areaEffectCloudEntity);
     }
@@ -101,6 +92,11 @@ public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEn
     @Override
     public boolean hasNoGravity() {
         return true;
+    }
+
+    @Override
+    protected void initDataTracker() {
+
     }
 
     @Override
@@ -114,7 +110,7 @@ public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEn
         int i = color;
         double d = (double)(i >> 16 & 0xFF) / 255.0;
         double e = (double)(i >> 8 & 0xFF) / 255.0;
-        double f = (double)(i & 0xFF) / 255.0;
+        double f = (double)(i >> 0 & 0xFF) / 255.0;
         for (int j = 0; j < amount; ++j) {
             getWorld().addParticle(ParticleTypes.ENTITY_EFFECT, getParticleX(0.5), getRandomBodyY(), getParticleZ(0.5), d, e, f);
         }
@@ -135,4 +131,3 @@ public class AcidBlastEntity extends PersistentProjectileEntity implements GeoEn
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 }
-
