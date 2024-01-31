59558a5720045c517c9ea0c6d62eda8324dd4915
NordAct <73118477+NordAct@users.noreply.github.com>
2024-02-01 01:14:02 +0300

    Bump dependencies and loom versions, replace Redirect for InGameHudMixin with WrapOperation, fix dependencies

--- src/main/java/nordmods/uselessreptile/common/mixin/client/InGameHudMixin.java
+++ src/main/java/nordmods/uselessreptile/common/mixin/client/InGameHudMixin.java
@@ -1,5 +1,7 @@
 package nordmods.uselessreptile.common.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.Perspective;
@@ -7,12 +9,12 @@ import nordmods.uselessreptile.common.entity.base.URRideableDragonEntity;
 import nordmods.uselessreptile.common.init.URConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-    @Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/Perspective;isFirstPerson()Z"))
-    private boolean render(Perspective instance) {
-        return instance.isFirstPerson() || URConfig.getConfig().enableCrosshair && MinecraftClient.getInstance().player.getVehicle() instanceof URRideableDragonEntity;
+    @WrapOperation(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/Perspective;isFirstPerson()Z"))
+    private boolean render(Perspective instance, Operation<Boolean> original) {
+        if (URConfig.getConfig().enableCrosshair && MinecraftClient.getInstance().player.getVehicle() instanceof URRideableDragonEntity) return true;
+        return original.call(instance);
     }
 }
