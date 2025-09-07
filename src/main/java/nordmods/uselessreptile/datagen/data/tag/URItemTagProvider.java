80727bfd9b70ce338071bd99506fd0f911d5605b
Nord_Act <73118477+NordAct@users.noreply.github.com>
2025-09-07 15:45:31 +0300

    Cleanup tags

--- src/main/java/nordmods/uselessreptile/datagen/data/tag/URItemTagProvider.java
+++ src/main/java/nordmods/uselessreptile/datagen/data/tag/URItemTagProvider.java
@@ -89,15 +89,5 @@ public class URItemTagProvider extends FabricTagProvider.ItemTagProvider{
         getTagBuilder(URTags.MAGMAMUNCHER_FOOD)
                 .add(Items.MAGMA_BLOCK.getRegistryEntry().registryKey().getValue())
                 .add(Items.MAGMA_CREAM.getRegistryEntry().registryKey().getValue());
-
-        //Dragon taming items
-        getTagBuilder(URTags.WYVERN_TAMING_ITEM)
-                .addOptionalTag(URTags.WYVERN_FOOD.id());
-        getTagBuilder(URTags.RIVER_PIKEHORN_TAMING_ITEM)
-                .add(Items.TROPICAL_FISH_BUCKET.getRegistryEntry().registryKey().getValue());
-        getTagBuilder(URTags.MOLECLAW_TAMING_ITEM)
-                .addOptionalTag(URTags.MOLECLAW_FOOD.id());
-        getTagBuilder(URTags.MAGMAMUNCHER_TAMING_ITEM)
-                .add(Items.BLAZE_ROD.getRegistryEntry().registryKey().getValue());
     }
 }
