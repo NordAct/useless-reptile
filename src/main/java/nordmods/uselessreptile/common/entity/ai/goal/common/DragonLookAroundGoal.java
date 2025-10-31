17c7db118874ccbc5132b63ff3d6dcc83f8d1664
Nord_Act <73118477+NordAct@users.noreply.github.com>
2025-10-31 22:17:14 +0300

    Fix seizures

--- src/main/java/nordmods/uselessreptile/common/entity/ai/goal/common/DragonLookAroundGoal.java
+++ src/main/java/nordmods/uselessreptile/common/entity/ai/goal/common/DragonLookAroundGoal.java
@@ -1,19 +1,51 @@
 package nordmods.uselessreptile.common.entity.ai.goal.common;
 
-import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import nordmods.uselessreptile.common.entity.base.ShooterDragon;
 import nordmods.uselessreptile.common.entity.base.URDragonEntity;
 
-public class DragonLookAroundGoal extends LookAroundGoal {
-    private final URDragonEntity mob;
+import java.util.EnumSet;
+
+public class DragonLookAroundGoal extends Goal {
+    protected final URDragonEntity mob;
+    protected int lookTimer;
+    protected float x;
+    protected float z;
 
     public DragonLookAroundGoal(URDragonEntity mob) {
-        super(mob);
         this.mob = mob;
+        setControls(EnumSet.of(Control.MOVE, Control.LOOK));
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        float angle = mob.getRandom().nextFloat() * MathHelper.PI * 2;
+        x = MathHelper.cos(angle);
+        z = MathHelper.sin(angle);
+        lookTimer = 20 + mob.getRandom().nextInt(20);
     }
 
     @Override
     public boolean canStart() {
-        if (this.mob.isDancing()) return false;
-        return super.canStart();
+        if (mob.isDancing()) return false;
+        return mob.getRandom().nextFloat() < 0.02;
+    }
+
+    @Override
+    public boolean shouldContinue() {
+        return lookTimer >= 0;
+    }
+
+    @Override
+    public void tick() {
+        --lookTimer;
+        if (mob instanceof ShooterDragon shooterDragon) {
+            Vec3d point = shooterDragon.getShootingPoint().pos();
+            mob.getLookControl().lookAt(point.x + x, point.y, point.z + z);
+        }
+        else mob.getLookControl().lookAt(mob.getX() + x, mob.getEyeY(), mob.getZ() + z);
     }
 }
